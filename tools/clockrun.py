52f7cd538175f1eb2bfa4a0841108aa9e77c9067
Wwupup <wuweimyself@qq.com>
2022-10-28 15:38:05 +0800

    add clock script

---
+++ tools/clockrun.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+from time import sleep
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Convert wwfacedet models to libfacedetect dnn data')
+    parser.add_argument('cur_config', help='current config file path')
+    parser.add_argument('target_config', help='target config file path')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    root = '../work_dirs/'
+    target_dir = os.path.join(
+        root,
+        os.path.basename(args.cur_config).replace('.py', ''))
+    assert not os.path.exists(target_dir)
+    print(f'Waiting for {target_dir} ...')
+    target = os.path.join(target_dir, 'epoch_640.pth')
+    while (not os.path.exists(target)):
+        sleep(3600)
+    print('OK!')
+    os.system(f'bash ./tools/dist_train.sh {args.target_config} 2 12347')
